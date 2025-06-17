233f25c21f15621569b5d59fd271862fea3bd000
RYUSNEY <ruapazap@est.unap.edu.pe>
2025-06-16 22:02:30 -0500

    Codigo corregido y funcional

--- resources/views/docentes.blade.php
+++ resources/views/docentes.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Docentes</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/sass/app.scss'])
 </head>
 <body>
     <div id="app">
